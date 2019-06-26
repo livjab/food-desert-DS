28d5f8829f05f3b6ceeef8d253342932b1c6c958
Liv Johnson <livjab@gmail.com>
2019-06-26 10:58:46 -0500

    getting lat/lon values from address

---
+++ geocoding.py
@@ -0,0 +1,9 @@
+from pygeocoder import Geocoder
+import pandas as pd
+import numpy as np
+
+if:
+    Geocoder.geocode("""address entry""").valid_address()
+    print(results.coordinates)
+else:
+    print("Not a valid address")    
